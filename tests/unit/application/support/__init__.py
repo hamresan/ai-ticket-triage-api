44961fd63a7258b9dfd4ccc0c88658081d393843
hamresan <pourshahmir@gmail.com>
2026-09-24 22:33:27 +0400

    refactor: remove speculative stage one contracts

--- tests/unit/application/support/__init__.py
+++ tests/unit/application/support/__init__.py
@@ -1,3 +1,4 @@
-from tests.unit.application.support.fakes import InMemoryTicketRepository, ScriptedTriageProvider
+from tests.unit.application.support.in_memory_ticket_repository import InMemoryTicketRepository
+from tests.unit.application.support.scripted_triage_provider import ScriptedTriageProvider
 
 __all__ = ["InMemoryTicketRepository", "ScriptedTriageProvider"]
