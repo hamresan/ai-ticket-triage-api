0992bd70862f6931508d7fb0fa788bdc355bb638
hamresan <pourshahmir@gmail.com>
2026-09-24 22:24:19 +0400

    refactor: expose ticket validation policies

--- src/ai_ticket_triage/domain/tickets/policies/__init__.py
+++ src/ai_ticket_triage/domain/tickets/policies/__init__.py
@@ -1,3 +1,4 @@
+from ai_ticket_triage.domain.tickets.policies.text_validation import TicketTextValidationPolicy
 from ai_ticket_triage.domain.tickets.policies.transitions import TicketTransitionPolicy
 
-__all__ = ["TicketTransitionPolicy"]
+__all__ = ["TicketTextValidationPolicy", "TicketTransitionPolicy"]
