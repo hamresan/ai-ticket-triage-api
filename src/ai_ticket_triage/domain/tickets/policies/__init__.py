b0843271f3f7434ce477269c49c5fc6e54f2eb70
hamresan <pourshahmir@gmail.com>
2026-09-24 22:24:39 +0400

    refactor: keep policy package initialization lightweight

--- src/ai_ticket_triage/domain/tickets/policies/__init__.py
+++
@@ -1,4 +0,0 @@
-from ai_ticket_triage.domain.tickets.policies.text_validation import TicketTextValidationPolicy
-from ai_ticket_triage.domain.tickets.policies.transitions import TicketTransitionPolicy
-
-__all__ = ["TicketTextValidationPolicy", "TicketTransitionPolicy"]
