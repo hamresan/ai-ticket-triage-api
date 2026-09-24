00f0318ff4892b464c4d0c40690358d3756dd5f2
hamresan <pourshahmir@gmail.com>
2026-09-24 23:14:02 +0400

    feat: export triage response schema

--- src/ai_ticket_triage/presentation/schemas/__init__.py
+++ src/ai_ticket_triage/presentation/schemas/__init__.py
@@ -1,4 +1,13 @@
 from ai_ticket_triage.presentation.schemas.ticket_filters import TicketStatusQuery
-from ai_ticket_triage.presentation.schemas.tickets import CreateTicketRequest, TicketResponse
+from ai_ticket_triage.presentation.schemas.tickets import (
+    CreateTicketRequest,
+    TicketResponse,
+    TriageDecisionResponse,
+)
 
-__all__ = ["CreateTicketRequest", "TicketResponse", "TicketStatusQuery"]
+__all__ = [
+    "CreateTicketRequest",
+    "TicketResponse",
+    "TicketStatusQuery",
+    "TriageDecisionResponse",
+]
