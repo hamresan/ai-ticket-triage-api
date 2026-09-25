573227d345b7f352144518197f5ec1c4762d4f56
hamresan <pourshahmir@gmail.com>
2026-09-25 09:39:40 +0400

    test: export structured log capture support

--- tests/integration/presentation/support/__init__.py
+++ tests/integration/presentation/support/__init__.py
@@ -2,5 +2,6 @@ from tests.integration.presentation.support.client import (
     build_client,
     build_test_application,
 )
+from tests.integration.presentation.support.logging import ListLogHandler
 
-__all__ = ["build_client", "build_test_application"]
+__all__ = ["ListLogHandler", "build_client", "build_test_application"]
